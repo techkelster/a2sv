c75c9c939a0159c8786967e6eaa41bc1d8c8c78a
ABDULMEJD KELIL SHIFA <111517328+techkelster@users.noreply.github.com>
2023-12-28 12:23:48 +0300

    Added solution for Sort the People

--- progress/sort-the-people.py
+++ progress/sort-the-people.py
@@ -1,10 +1,35 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        for i in range(len(heights)):
-            for j in range(len(heights) - 1):
-                if heights[j] < heights[j + 1]:
-                    heights[j], heights[j + 1] = heights[j + 1], heights[j]
-                    names[j], names[j+1] = names[j + 1], names[j]
-        return names
+        size = len(heights)
+        # for i in range(size):
+        #     indMax = i 
+        #     for j in range(i + 1, size):
+        #         if heights[j] > heights[indMax]:
+        #             indMax = j
+        #     heights[i], heights[indMax] = heights[indMax], heights[i]
+        #     names[i], names[indMax] = names[indMax], names[i]
+        # return names
+
+        # for i in range(1, size):
+        #     j = i
+        #     while j >= 0 and heights[i] < heights[j]:
+        #         j -= 1
+        #     heights[i], heights[j]  = heights[i], heights
+
+        storage = [0] * (max(heights) + 1)
+        ans = []
+
+        mapper = dict(zip(heights, names))
+
+        for i in heights:
+            storage[i] += 1
+        for j in range(len(storage)):
+            ans = [j] * storage[j] + ans
+       
+        return [mapper[i] for i in ans]
+        
+
+
+                    
 
         
