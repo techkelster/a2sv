4a92478e9f8620c5058b2039a5a18c4a1c94b263
ABDULMEJD KELIL SHIFA <111517328+techkelster@users.noreply.github.com>
2024-01-03 17:36:14 +0300

    Added solution for Largest Number

---
+++ progress/largest-number.py
@@ -0,0 +1,24 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        lst = []
+
+        for ele in nums:
+            lst += [str(ele)]
+        
+        n = len(lst)
+
+        for i in range(n):
+            for j in range(i+1 , n):
+                
+                if str(lst[i]) + str(lst[j]) > str(lst[j]) + str(lst[i]):
+                    continue
+                else:
+                    lst[i] , lst[j] = lst[j] , lst[i]
+        
+        
+        ans = ''.join(lst)
+
+        if int(ans) == 0:
+            return "0"
+        
+        return ans
