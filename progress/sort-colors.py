94144897dbb3e0f458255b5f6f67fcc138388a36
ABDULMEJD KELIL SHIFA <111517328+techkelster@users.noreply.github.com>
2024-02-03 02:27:53 +0300

    Added solution for Sort Colors

--- progress/sort-colors.py
+++ progress/sort-colors.py
@@ -1,10 +1,19 @@
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+    def sortColors(self, a: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            for j in range(i, len(nums)):
-                if nums[j] < nums[i]:
-                    nums[j], nums[i] = nums[i], nums[j]
-        
+        m = 0
+        l = 0
+        h = len(a) - 1
+        while m <= h:
+            if a[m] == 0:
+                a[m], a[l] = a[l], a[m]
+                m += 1
+                l += 1
+            elif a[m] == 1:
+                m += 1
+            else:
+                a[m], a[h] = a[h], a[m]
+                h -= 1
+        return a
