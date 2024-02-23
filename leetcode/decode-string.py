745e7f5ec87db8c533a93429c5845009b3827f12
ABDULMEJD KELIL SHIFA <111517328+techkelster@users.noreply.github.com>
2024-02-23 15:28:40 +0300

    Add solution for Decode String

---
+++ leetcode/decode-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        res,num = "", 0
+        st = []
+        for c in s:
+            if c.isdigit():
+                num = num*10+int(c)    
+            elif c=="[":
+                st.append(res)
+                st.append(num)
+                res=""
+                num=0
+            elif c=="]":
+                pnum = st.pop()
+                pstr = st.pop()
+                res = pstr + pnum*res
+            else:
+                res+=c
+        
+        return res
+
+    #copied and understood.
+	
