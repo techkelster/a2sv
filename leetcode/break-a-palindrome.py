e1e6c98db70d592b7f6b8a462518d81f5a430fcb
ABDULMEJD KELIL SHIFA <111517328+techkelster@users.noreply.github.com>
2024-02-27 10:10:46 +0300

    Add solution for Break a Palindrome

---
+++ leetcode/break-a-palindrome.py
@@ -0,0 +1,12 @@
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        size = len(palindrome)
+        if size == 1:
+            return ''
+     
+        for i in range(size // 2):
+            if palindrome[i] != 'a':
+                return palindrome[:i] + 'a' + palindrome[i + 1:]
+        return palindrome[:-1] + 'b'
